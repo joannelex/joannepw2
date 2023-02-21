7facf8c168931f6162c41b53b26e1e228cd358c8
joanne <jlee2002@students.cs.ubc.ca>
2023-02-20 18:51:25 -0800

    added artAndGallery

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import avatar from './assets/avatar.svg';
 import { DateTime } from './components/DateTime';
 import { About } from './components/About';
 import { Projects } from './components/Projects';
+import { ArtAndGallery } from './components/ArtAndGallery';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowUp } from '@fortawesome/free-solid-svg-icons'
 
@@ -30,20 +31,21 @@ function App() {
       </div>
 
       <div id='about-page'>
-        <div className='section-header'> &lt; about &gt; </div>
+        <div className='section-header'> &lt;about&gt; </div>
         <About/>
-        <div className='section-header'> &lt; /about &gt; </div>
+        <div className='section-header'> &lt;/ about&gt; </div>
       </div>
 
       <div id='projects-page'>
-        <div className='section-header'> &lt; projects &gt; </div>
+        <div className='section-header'> &lt;projects&gt; </div>
         <Projects/>
-        <div className='section-header'> &lt; /projects &gt; </div>
+        <div className='section-header'> &lt;/ projects&gt; </div>
       </div>
 
       <div id='artAndGallery-page'>
-        <div className='section-header'> &lt; artAndGallery &gt; </div>
-        <div className='section-header'> &lt; /artAndGallery &gt; </div>
+        <div className='section-header'> &lt;artAndGallery&gt; </div>
+        <ArtAndGallery />
+        <div className='section-header'> &lt;/ artAndGallery&gt; </div>
       </div>
 
       <a href="#topnav">
---
+++ src/components/ArtAndGallery.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import './ArtAndGallery.css'
+import ipad from '../assets/ipad.svg'
+import jennie from '../assets/jennie.png'
+import naeun from '../assets/naeun.png'
+import rose from '../assets/rose.png'
+
+import magazine from '../assets/magazine.png'
+import mirror from '../assets/mirror.png'
+import jam from '../assets/jam.png'
+
+export const ArtAndGallery = () => {
+
+    return(
+        <div className="art-container">
+            <div className='gallery-text'>
+            The harmony between fine lines and colours never fail to amuse me. From landscapes to animals and figures, 
+            I like to grab my pen and sculpt my mind on the paper during my free time. Lately, I’ve been into figure 
+            drawing- gere are some of my recent digital works!
+            </div>
+
+            <div className='gallery-container'>
+                <img src={ ipad } id="ipad" alt="ipad"/>
+                <div className='drawings'>
+                    <div>
+                        <img src={ naeun } className="sketch" id="naeun" alt="naeun"/>
+                        <img src={ rose } className="sketch" alt="rose"/>
+                        <img src={ jennie } className="sketch" alt="jennie"/>
+                    </div>
+
+                    <div className='digital'>
+                        <img src={ magazine } className="digital-drawing" alt="magazine"/>
+                        <img src={ mirror } className="digital-drawing" id="mirror" alt="mirror"/>
+                        <img src={ jam } className="digital-drawing" id="jam" alt="jam"/>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ArtAndGallery
